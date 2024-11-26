372f1cd1ea6cda1dd0fba7c23b53e7559d7e56b4
Davide De Pasquale <davide.depasquale1998@gmail.com>
2024-11-26 19:31:17 +0100

    finita

--- src/components/AllTheBook.jsx
+++ src/components/AllTheBook.jsx
@@ -1,22 +1,71 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-function AllTheBook() {
-  return (
-    <Container>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </Container>
-  );
+import horror from "../books/horror.json";
+import fantasy from "../books/fantasy.json";
+import history from "../books/history.json";
+import romance from "../books/romance.json";
+import scifi from "../books/scifi.json";
+
+import { Component } from "react";
+
+class AllTheBook extends Component {
+  state = {
+    books: fantasy
+  };
+  render() {
+    console.log("render");
+    return (
+      <Container>
+        <div className="my-5 text-center">
+          <Button
+            className="mx-2 bg-success"
+            onClick={() => this.setState({ books: horror })}
+          >
+            Horror
+          </Button>
+          <Button
+            className="mx-2 text-bg-light"
+            onClick={() => this.setState({ books: fantasy })}
+          >
+            Fantasy
+          </Button>
+          <Button
+            className="mx-2 bg-danger"
+            onClick={() => this.setState({ books: history })}
+          >
+            History
+          </Button>
+          <Button
+            className="mx-2 text-bg-dark"
+            onClick={() => this.setState({ books: scifi })}
+          >
+            Scifi
+          </Button>
+          <Button
+            className="mx-2 bg-purple"
+            onClick={() => this.setState({ books: romance })}
+          >
+            Romance
+          </Button>
+        </div>
+        <Row xs={1} sm={1} md={2} lg={3} xl={4}>
+          {this.state.books.map((book) => (
+            <Col key={book.asin}>
+              <Card bg="light">
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.price}€</Card.Text>
+                  <Button variant="primary">{book.category}</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default AllTheBook;
